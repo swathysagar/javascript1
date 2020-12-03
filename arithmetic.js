c0c518542b81d14d6c1316b166accf28692e5e40
swathy sagar <swathysagar.legacy@gmail.com>
2020-12-03 13:02:03 +0530

    this is my first javascriptprograms

---
+++ arithmetic.js
@@ -0,0 +1,31 @@
+console.log(10+20)
+console.log(2**8)
+console.log(2^4)
+var num=-10;
+if(num>0){
+    console.log(+num+ " is a positive number");
+}
+else{
+    console.log(+num+ "is a negative number");
+}
+if(num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+var num2=0;
+var num1=50;
+var sum=num1+num2;
+if(sum==50|num1==50|num2==50){
+    console.log("true");
+}
+else{
+    console.log("false");
+}
+if(num1>num2){
+    console.log("largest number:" +num1);
+}
+else{
+    console.log("largest number:" +num2);
+}
---
+++ hel.js
@@ -0,0 +1,11 @@
+console.log("hello world.....")
+var name="swathy sagar";
+var age=24;
+console.log("hai" +name+ "you are"+age+"years old")
+var num1=10;
+var num2=20;
+var temp=num1;
+num1=num2;
+num2=temp;
+console.log("value after swapping ,num1 :" +num1)
+console.log("value after swapping ,num2 :" +num2)
